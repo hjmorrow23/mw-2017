f35bf563a9a7ce1c9584afabe69652221d776707
hjmorrow23 <hjmorrow23@gmail.com>
2017-03-28 20:42:20 -0400

    Added responsive nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require("gulp");
 
 var sass = require("gulp-sass");
+var minifycss = require("gulp-minify-css");
+var browserSync = require("browser-sync").create();
+var plumber = require("gulp-plumber");
+var notifier = require("node-notifier");
+var notify = require("gulp-notify");
+
+// BrowserSync server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'));
+    	.pipe(plumber())
+        .pipe(sass()
+            .on("error", notify.onError({
+                title: "Sass Error",
+                message: "Error: <%= error.message %>"
+            }))
+        )
+        .pipe(minifycss())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['sass', 'browser-sync'], function() {
     gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
 });
+
+
 
